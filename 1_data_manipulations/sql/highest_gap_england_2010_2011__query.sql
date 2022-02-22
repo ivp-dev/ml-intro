41ff55a58feb631a655dbe214f00f122130e63cd
ivp.dev <ivp.dev@outlook.com>
2022-02-22 18:06:45 +0300

    task 15 done

--- 1_data_manipulations/sql/highest_gap_england_2010_2011__query.sql
+++ 1_data_manipulations/sql/highest_gap_england_2010_2011__query.sql
@@ -1,3 +1,3 @@
-SELECT match_api_id, home_team_api_id as team_api_id, date FROM `Match`
+SELECT match_api_id, home_team_api_id as team_api_id, date FROM `Match` WHERE league_id = 1729 AND season = '2010/2011'  
 UNION ALL
-SELECT match_api_id, away_team_api_id as team_api_id, date FROM `Match`
+SELECT match_api_id, away_team_api_id as team_api_id, date FROM `Match` WHERE league_id = 1729 AND season = '2010/2011'
